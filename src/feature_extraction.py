84c2c75e3da18b78fdb379e81eddb3718d52bb3f
Kris Erickson <kristian.erickson@gmail.com>
2025-06-19 07:12:06 -0700

    Add itemprop to the features set.

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,8 +1,9 @@
 import re
 from typing import Any, Dict, List, Tuple, Union
+
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline
+from sklearn.pipeline import FeatureUnion, make_pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 
 # Measurement units for English recipes
@@ -111,6 +112,7 @@ def preprocess_data(features: List[Dict[str, Any]]) -> List[Tuple[Dict[str, Any]
     combined = list(zip(features_wo_text, texts))
     return combined
 
+
 def extract_features(el: Dict[str, Any], idx: int, elements: list[Dict[str, Any]], current_section_heading: str | None = None) -> Dict[str, Any]:
     """
     Extracts structured features from a list of elements.
@@ -130,6 +132,10 @@ def extract_features(el: Dict[str, Any], idx: int, elements: list[Dict[str, Any]
     class_id_str = " ".join(str(x) for x in el.get("class", [])) + " " + str(el.get("id", ""))
     class_id_str = class_id_str.lower()
 
+    # Microdata
+    itemprop = el.get("itemprop", "")
+    itemprop = itemprop.lower() if itemprop else ""
+
     features = {
         "tag": el["tag"],
         "depth": el["depth"],
@@ -148,7 +154,10 @@ def extract_features(el: Dict[str, Any], idx: int, elements: list[Dict[str, Any]
         "is_under_current_direction_section": int(current_section_heading == "direction"),
         "class_has_ing_keyword": int(any(k in class_id_str for k in ing_keywords)),
         "class_has_dir_keyword": int(any(k in class_id_str for k in dir_keywords)),
-        "class_has_title_keyword": int(any(k in class_id_str for k in title_keywords))
+        "class_has_title_keyword": int(any(k in class_id_str for k in title_keywords)),
+        "itemprop_name": int(itemprop == "name"),
+        "itemprop_ingredient": int(itemprop == "recipeingredient"),
+        "itemprop_instructions": int(itemprop == "recipeinstructions"),
     }
 
 
